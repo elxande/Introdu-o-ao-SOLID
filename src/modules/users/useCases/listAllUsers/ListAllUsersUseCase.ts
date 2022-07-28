91c864800adca6fc4cb65be0eeb0d0236ac00e7a
Alexandre Oliveira <elxande.10@gmail.com>
2022-07-28 17:53:01 -0300

    desafio concluído

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,14 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const userAlreadyExists = this.usersRepository.findById(user_id);
+    if (!userAlreadyExists) {
+      throw new Error("User not found");
+    }
+    if (userAlreadyExists.admin === false) {
+      throw new Error("User not Admin Fpund");
+    }
+    return this.usersRepository.list();
   }
 }
 
